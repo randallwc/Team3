a23f856cfe6e4826a6be69c42ec0bd437b8dcc3a
William Randall <15082470+randallwc@users.noreply.github.com>
2022-03-05 17:57:53 -0800

    update camera iface class

--- CameraIface.py
+++ CameraIface.py
@@ -1,13 +1,20 @@
-import Exceptions
-
-
-# TODO -- create this class
 class CameraIface:
-    def __init__(self):
-        pass
+    def __init__(self, camera_width, camera_height, num_levels=3):
+        # ... initialize the class with constant values here ...
+        self.camera_width = camera_width
+        self.camera_height = camera_height
+        self.num_levels = num_levels
+        # ... add member variables here and if you don't know what they should be set to then say
+        # self.variable_name = None
 
     def calibrate(self):
-        raise Exceptions.NotImplementedException
+        # ... create a function that enters calibration mode here ...
+        # ... then store that calibration in the class ...
+        pass
 
     def get_object_position(self):
-        raise Exceptions.NotImplementedException
+        # ... create a function that returns the x, y position of the ball here ...
+        x = 0
+        y = 0
+        # ... write the code that takes a picture and then returns the center position of the ball ...
+        return x, y
