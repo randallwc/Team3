95565779132b6f370cac5aaf9c21e601a7b5ab52
jcr7467 <25701682+jcr7467@users.noreply.github.com>
2022-04-07 15:39:47 -0700

    Incrase maxhttpbuffersize 1e6

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ const cors = require("cors");
 // initializing socket.io
 const io = require("socket.io")(server, {
     pingTimeout: 300000,
-    maxHttpBufferSize: 5e5,
+    maxHttpBufferSize: 1e6,
     //pingInterval: 70000
 });
 
