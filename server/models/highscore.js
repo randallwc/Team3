1596ec42079123b61bb3c0c1c9cffb72df59ed8e
jcr7467 <25701682+jcr7467@users.noreply.github.com>
2022-04-19 17:44:11 -0700

    Deleted dbiface, added highscore sorting funcs

--- server/models/highscore.js
+++ server/models/highscore.js
@@ -1,26 +1,104 @@
 // Will be used to store our scores
 const mongoose = require("mongoose");
+const masterKey = 'MasterKey';
 
 let HighscoreSchema = new mongoose.Schema({
-    singleGameHighscores1: [{
-        score: Number,
-    }],
-    lifetimeGameHighscores1: [{
-        score: Number,
-        username: String
-    }],
     singleGameHighscores: [{
-        playerID: Number,
+        playerID: mongoose.ObjectId,
+        username: String,
+        score: Number
     }],
     lifetimeGameHighscores: [{
-        score: Number,
-        username: String
+        playerID: mongoose.ObjectId,
+        username: String,
+        score: Number
     }],
+    key: {
+        // master key will be 'MasterKey'
+        type: String,
+        unique: true
+    }
 });
 
-HighscoreSchema.statics.handleSingleGameScore = function(){}
+const sortByScore = (first, second) => {
+    if (first.score > second.score) {
+        return -1;
+    }else if (first.score < second.score) {
+        return 1;
+    }
+    return 0;
+};
 
-HighscoreSchema.statics.handleScoreForLifetimeScore = function(){}
+
+HighscoreSchema.statics.handleSingleGameScore = function(playerID, username, score){
+    return new Promise((resolve, reject) => {
+
+        Highscore.findOne({key: masterKey}).then(highscores => {
+            let {singleGameHighscores} = highscores;
+
+            // find if score is already in array
+            //1 if it is, chheck score to see if given score is higher, if so, and sort
+            let scorerFound = false;
+            for(const scoreObject of singleGameHighscores){
+                const thisIsTheUser = scoreObject.username === username && scoreObject.playerID.toString() === playerID;
+                const userScoredHigherThisTime = scoreObject.score < score;
+                if (thisIsTheUser && userScoredHigherThisTime){
+                    scoreObject.score = score;
+                    scorerFound = true;
+                }
+            }
+
+            //2 if it isn't, append to array and sort
+            if (!scorerFound){
+                singleGameHighscores.push({
+                    playerID: playerID,
+                    username: username,
+                    score: score
+                });
+            }
+
+            singleGameHighscores.sort(sortByScore);
+            return highscores.save();
+        }).then(highscores => {
+            resolve(highscores);
+        }).catch(err => {
+            reject(err);
+        })
+    });
+}
+
+HighscoreSchema.statics.handleScoreForLifetimeScore = function(playerID, username, score){
+    Highscore.findOne({key: masterKey}).then(highscores => {
+        const {lifetimeGameHighscores} = highscores;
+
+        // find if score is already in array
+        //1 if it is, add to the score and sort
+        let scorerFound = false;
+        for(const scoreObject of lifetimeGameHighscores){
+            const thisIsTheUser = scoreObject.username === username && scoreObject.playerID.toString() === playerID;
+            if (thisIsTheUser){
+                scoreObject.score += score;
+                scorerFound = true;
+            }
+        }
+
+        //2 if it isn't, append to array and sort
+        if (!scorerFound){
+            lifetimeGameHighscores.push({
+                playerID: playerID,
+                username: username,
+                score: score
+            });
+        }
+
+        lifetimeGameHighscores.sort(sortByScore);
+        return highscores.save();
+    }).then(highscores => {
+        return resolve(highscores);
+    }).catch(err => {
+        return reject(err);
+    })
+}
 
 HighscoreSchema.statics.fetchNHighestSingleGameScores = function(nScores){
     return new Promise((resolve, reject) => {
