cdb6f9b2f5e17539e93bfdea1e5eb78ff880dddc
William Randall <15082470+randallwc@users.noreply.github.com>
2022-05-20 19:36:18 -0700

    test python file for testing imports

---
+++ client/test.py
@@ -0,0 +1,18 @@
+import cv2
+import dlib
+import json
+import math
+import os
+import paho.mqtt.client as mqtt
+import pygame
+import pygame.surface
+import pygame_gui
+import pyttsx3
+import random
+import requests
+import socketio
+import socketio.exceptions
+import speech_recognition as sr
+import sys
+import time
+import uuid
