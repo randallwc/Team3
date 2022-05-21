c5f818fcb2a876469bff2595f334d60a0a4ac099
William Randall <15082470+randallwc@users.noreply.github.com>
2022-05-20 20:04:18 -0700

    update test.py

--- client/test.py
+++ client/test.py
@@ -1,18 +1,23 @@
-import cv2
-import dlib
 import json
 import math
 import os
+import random
+import sys
+import time
+import uuid
+
+import cv2
+import dlib
 import paho.mqtt.client as mqtt
 import pygame
 import pygame.surface
 import pygame_gui
 import pyttsx3
-import random
 import requests
 import socketio
 import socketio.exceptions
 import speech_recognition as sr
-import sys
-import time
-import uuid
+
+engine = pyttsx3.init()
+engine.say("working download")
+engine.runAndWait()
